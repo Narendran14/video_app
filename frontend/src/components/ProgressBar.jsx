7a3625789413f340614a6e84e23316ec5fec2a43
Magesh <156405930+Narendran14@users.noreply.github.com>
2025-11-11 10:57:01 +0530

    Delete frontend directory

--- frontend/src/components/ProgressBar.jsx
+++
@@ -1,7 +0,0 @@
-export default function ProgressBar({ percent }) {
-  return (
-    <div className="w-full bg-gray-200 rounded h-4 overflow-hidden">
-      <div style={{ width: `${percent}%` }} className="h-4 bg-blue-600 transition-all"></div>
-    </div>
-  );
-}
